9df1b44fa48ef69764a08d4354a31f485cf793f5
devhector <hectorhu17@gmail.com>
2023-03-13 17:57:26 -0300

    feat: create phase and add coins

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ from classes import *
 from _thread import *
 
 FPS = 60
-WIDTH = 800
+WIDTH = 900
 HEIGHT = int(WIDTH * 0.8)
 BACKGROUND = (100, 42, 0)
 
@@ -16,6 +16,32 @@ def create_phase(boxes):
 	boxes.add(Box(360, 500))
 	boxes.add(Box(100, 70))
 
+def create_phase2(boxes):
+	#caixas inferiores esquerda
+	for bx in range(40, 170, 70):
+		boxes.add(Box(bx, 670))
+	
+	#caixas superiores direita 
+	for bx in range(650, 900, 70):
+		boxes.add(Box(bx, 110))
+
+	#caixas a esquerda
+	for by in range(110, 680, 70):
+		boxes.add(Box(580, by))
+
+	b = [3, 7]
+	for i in b:
+		a = (i * 70) + 180
+		boxes.add(Box(860, a))
+		boxes.add(Box(650, a))
+
+	boxes.add(Box(40, 410))
+	boxes.add(Box(40, 110))
+	boxes.add(Box(240, 260))
+	boxes.add(Box(240, 565))
+	boxes.add(Box(450, 670))
+	boxes.add(Box(510, 180))
+
 def update_users(data, other):
 	data = data[2:-2]
 	data = data.split(",")
@@ -25,6 +51,20 @@ def update_users(data, other):
 	other.w_index = int(data[4])
 	other.update()
 	return other
+
+def get_coins(data):
+	coins = pygame.sprite.Group()
+	for coin in data:
+		coin = coin.split(",")
+		coins.add(
+			Coin(
+					int(coin[1]),
+					int(coin[2]),
+					int(coin[0]),
+					coin[3] == "True"
+				)
+		)
+	return coins
 
 def parser(data):
 	if data == "":
@@ -47,19 +87,28 @@ def main():
 	textRect = text.get_rect()
 	textRect.center = (WIDTH // 2, HEIGHT // 2)
 
-	player = Player(100, 200)
-	other = OtherPlayer(100, 200)
-	# coin = Coin(300, 100)
+	player = Player(40, 500)
+	other = OtherPlayer(0, 0)
 
 	flag = False
 
 	boxes = pygame.sprite.Group()
-	create_phase(boxes)
+	create_phase2(boxes)
 
 	player.listen_thread = start_new_thread(player.listen, ())
 
 	while True:
+		player.network.send("GET_COINS (\n\n)")
+		method, data = parser(player.data_players)
+		print (data)
+		if len(data) > 0:
+			if method == "COINS":
+				coins = get_coins(data)
+				break
 
+	while True:
+
+		print("data players: ", player.data_players)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
@@ -97,11 +146,11 @@ def main():
 			other.rect.y = HEIGHT + 50
 
 		screen.fill(BACKGROUND)
-		# coin.update()
-		# coin.draw(screen)
 		other.draw(screen)
 		player.update(boxes)
 		player.draw(screen)
+		coins.update()
+		coins.draw(screen)
 		boxes.draw(screen)
 		pygame.display.update()
 
